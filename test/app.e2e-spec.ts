650b1fe3294f8230a5b7134b5ca5a75e3c0ffb08
Abhishek Tiwari <abhishektiwariaman@gmail.com>
2021-10-06 11:20:52 +0530

    Update app.e2e-spec.ts
    Element Tracked

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,6 +3,8 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
+
+
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
